4291484cef807dc7b067f3fb9c0fcf301b9279bd
beverm2391 <beverman4528@gmail.com>
2023-10-06 11:01:44 -0400

    got aiolimiter example working

--- test-aiolimiter-v1.py
+++ test-aiolimiter-v1.py
@@ -2,13 +2,17 @@ import asyncio, time
 from aiolimiter import AsyncLimiter
 import pytest
 
-def test_aiolimiter():
+
+def main():
     limiter = AsyncLimiter(4, 8)
 
     async def _task(id):
         await asyncio.sleep(id * 0.01)
         async with limiter:
-            print(f'{id:>2d}: Drip! {time.time() - ref:>5.2f}')
-    
+            print(f'{id:>2d}: Drip! {time.time() - ref:>5.2f}s')
+
     tasks = [_task(i) for i in range(10)]
-    ref = time.time(); result = asyncio.run(asyncio.gather(*tasks)) 
+    ref = time.time(); result = asyncio.run(asyncio.wait(tasks)) 
+
+if __name__ == "__main__":
+    main()
