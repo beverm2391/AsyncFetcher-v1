4d83c3d8b959031888fc22021e432f67a22bc776
beverm2391 <beverman4528@gmail.com>
2023-10-10 13:07:34 -0400

    finished tests

--- tests/test_polygon.py
+++ tests/test_polygon.py
@@ -1,5 +1,7 @@
 import pytest
-from lib.utils import get_polygon_key
+from typing import List
+
+from lib.utils import get_polygon_key, get_nyse_date_tups
 from lib.polygon import make_url, validate, make_urls
 
 def test_make_url_sanity():
@@ -21,5 +23,12 @@ def test_validate():
         with pytest.raises(AssertionError):
             validate(invalid_list)
 
+
 def test_make_urls():
-    pass
+    def _has_direct_sublist(x): return any([isinstance(i, list) for i in x]) # check if list has a direct sublist (no good)
+
+    tickers = ['AAPL', 'MSFT']
+    tups = get_nyse_date_tups('2022-01-01', '2022-01-31')
+    urls = make_urls(tickers, tups)
+    assert isinstance(urls, list), "urls should be a list"
+    assert not _has_direct_sublist(urls), "urls should not have sublists"
